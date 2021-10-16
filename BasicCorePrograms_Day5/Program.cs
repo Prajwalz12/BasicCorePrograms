429e1d2f63851fee13917e0e414cd68e236b35be
Prajwal <prajwal.zurunge12@gmail.com>
2021-10-17 00:44:37 +0530

    adding quotient and remainder program

--- BasicCorePrograms_Day5/Program.cs
+++ BasicCorePrograms_Day5/Program.cs
@@ -4,23 +4,17 @@ namespace Csharpbasic
 {
     class Program
     {
-        public static void Harmonic(int num)
-        {
-
-            double result = 0;
-
-            for (float i = 1; i <= num; i++)
-            {
-                result += 1 / i;
-            }
-            Console.WriteLine(result);
-        }
-
         static void Main(string[] args)
         {
+            int dividend = 50, divisor = 8;
 
-            Program.Harmonic(4);
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
 
+            Console.WriteLine("Dividend:{0} Divisor:{1}", dividend, divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+            Console.ReadLine();
         }
 
     }
