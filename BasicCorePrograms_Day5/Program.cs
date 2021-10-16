e50253996fcb64477ea9c4e5dc559a61de7b4c65
Prajwal <prajwal.zurunge12@gmail.com>
2021-10-17 00:41:19 +0530

    adding powerof2 program

--- BasicCorePrograms_Day5/Program.cs
+++ BasicCorePrograms_Day5/Program.cs
@@ -4,15 +4,22 @@ namespace Csharpbasic
 {
     class Program
     {
+        public Program()
+        {
+            int num;
+            int result = 1;
+            num = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                result = 2 * i;
+            }
+            Console.WriteLine(result);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Year : ");
-            int Year = int.Parse(Console.ReadLine());
-            if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0))
-                Console.WriteLine("{0} is a Leap Year.", Year);
-            else
-                Console.WriteLine("{0} is not a Leap Year.", Year);
-            Console.ReadLine();
+            Program a = new Program();
+
         }
 
     }
