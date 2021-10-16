932eff8f72e2026b5a1bbbe210445f3a3e31dd51
Prajwal <prajwal.zurunge12@gmail.com>
2021-10-17 00:52:03 +0530

    adding even odd program

--- BasicCorePrograms_Day5/Program.cs
+++ BasicCorePrograms_Day5/Program.cs
@@ -1,24 +1,19 @@
-﻿using System;
+﻿sing System;
 
 namespace Csharpbasic
 {
     class Program
     {
-        public static void swap(int num1, int num2)
-        {
-            int num3 = 0;
-            Console.WriteLine($"Numbers before swapping are num1= {num1}and num2={num2}");
-            num3 = num1;
-            num1 = num2;
-            num2 = num1;
-            Console.WriteLine($"Numbers after swapping are num1={num1}and num2={num2}");
-        }
-
         static void Main(string[] args)
         {
+            int n;
+            Console.Write("Enter no", n);
+            if (n % 2 == 0)
 
-            Program.swap(5, 7);
+                Console.Write("No is Even");
 
+            else
+                Console.Write("No is odd");
         }
 
     }
